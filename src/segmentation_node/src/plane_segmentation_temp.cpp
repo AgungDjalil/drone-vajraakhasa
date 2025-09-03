72ef8a8df46b0d29230bdb4ae987c03a4a334266
AgungDjalil <agungfirmansyahdjalil@gmail.com>
2025-09-03 11:46:37 +0700

    up

--- src/segmentation_node/src/plane_segmentation_temp.cpp
+++ src/segmentation_node/src/plane_segmentation_temp.cpp
@@ -3,14 +3,18 @@
 #include <sensor_msgs/msg/point_cloud2.hpp>
 #include <pcl_conversions/pcl_conversions.h>
 #include <pcl/filters/passthrough.h>
+#include <pcl/filters/voxel_grid.h>
 
 class PlaneSegmentation : public rclcpp::Node 
 {
     public:
-        PlaneSegmentation() : Node("plane_segmentation")
+        PlaneSegmentation() : Node("plane_segmentation") // /zed/zed_node/point_cloud/cloud_registered
         {
             this->declare_parameter<std::string>("input_topic", "/zed/zed_node/point_cloud/cloud_registered");
             this->declare_parameter<std::string>("output_topic", "/segmented");
+            this->declare_parameter<double>("z_min", -5.0);
+            this->declare_parameter<double>("z_max", 5.0);
+            this->declare_parameter<double>("leaf_size", 0.03);
 
             const auto input = this->get_parameter("input_topic").as_string();
             const auto output = this->get_parameter("output_topic").as_string();
@@ -36,6 +40,7 @@ class PlaneSegmentation : public rclcpp::Node
 
             const double z_min   = this->get_parameter("z_min").as_double();
             const double z_max   = this->get_parameter("z_max").as_double();
+            const double leaf_size = this->get_parameter("leaf_size").as_double();
 
             pcl::PointCloud<pcl::PointXYZ>::Ptr region_of_interest_cloud (new pcl::PointCloud<pcl::PointXYZ>);
             {
@@ -46,8 +51,16 @@ class PlaneSegmentation : public rclcpp::Node
                 pass.filter(*region_of_interest_cloud);
             }
 
+            pcl::PointCloud<pcl::PointXYZ>::Ptr downsampling_cloud (new pcl::PointCloud<pcl::PointXYZ>);
+            {
+                pcl::VoxelGrid<pcl::PointXYZ> voxel_filter;
+                voxel_filter.setInputCloud(region_of_interest_cloud);
+                voxel_filter.setLeafSize(leaf_size, leaf_size, leaf_size);
+                voxel_filter.filter(*downsampling_cloud);
+            }
+
             sensor_msgs::msg::PointCloud2 output;
-            pcl::toROSMsg(*region_of_interest_cloud, output);
+            pcl::toROSMsg(*downsampling_cloud, output);
             output.header = msg->header;
             pub_->publish(output);
         }
